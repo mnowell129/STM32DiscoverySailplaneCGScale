d55797be006d40b83fe6e654ef70cae531cc48cf
nowelc <charles.m.nowell@medtronic.com>
2018-03-26 14:53:16 -0500

    Rev 1.0.0 CG Scale for STM32F429 Discovery Board. Using the same load cells and amps from the RC Groups thread. https://www.rcgroups.com/forums/showthread.php?2917114-CG-Scale-geek-way

---
+++ Inc/logo.h
@@ -0,0 +1,7 @@
+#ifndef LOGO_H
+#define LOGO_H
+#define x_logo_bmp 234 //width of the picture
+#define y_logo_bmp 270 //height of the picture
+extern const uint16_t pic_logo_bmp[];
+#endif
+
--- README.md
+++
@@ -1,2 +0,0 @@
-# STM32DiscoverySailplaneCGScale
-Redesign of the software and electronics for F5J/F3K CG Scale
